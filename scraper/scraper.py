61d5684a5915313b655e336c69f2cbe960b30e22
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2024-11-11 12:00:30 +0100

    Less log, better error handling

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -31,7 +31,7 @@ class TweetScraper:
 
         now = datetime.now()
         date_string = now.strftime("%Y-%m-%d_%H-%M-%S")
-        self.filename = f'test_{date_string}.csv'
+        self.filename = f'data/test_{date_string}.csv'
 
         self.url_to_scrape = f'https://x.com/search?f=live&q=(from%3A{self.account_name})%20until%3A{self.end_date}%20since%3A{self.begin_date}%20-filter%3Areplies&src=typed_query'
 
@@ -52,20 +52,40 @@ class TweetScraper:
         chrome = webdriver.Chrome(options=options)
         return chrome
 
+    def wait_for_connexion(self, chrome, url, max_nb_try=5, delay=30):
+            chrome.get(url)
+
+            time.sleep(5)
+
+            current_nb_try = 0
+            while True:
+                if current_nb_try >= max_nb_try:
+                    return False
+                try:
+                    chrome.find_element('xpath', '//button[@aria-label="Menu du compte"]')
+                    chrome.find_element('xpath', '//div[@role="presentation"]')
+                    break
+                except Exception as e:
+                    logging.debug(f"User is not connected, waiting for {delay} seconds before next check ({current_nb_try} try/{max_nb_try})")
+                    current_nb_try += 1
+                    time.sleep(delay)
+            return True
+
+
     def _init_scraping_process(self):
         logging.info("Init webdriver")
         chrome = self._init_webdriver()
-
+        logging.debug("Checking if user is connected...")
         if not self.wait_for_connexion(chrome, self.url_to_scrape):
+            logging.error("Max number of try reached to check user connected")
             chrome.quit()
-            raise Exception("User not connected")
+            raise Exception("User was not connected to Twitter, scrapin aborted")
+        logging.info("User is connected")
         return chrome
         
-
     def scrap_page_unitl_end_date(self, delay_between_page=5):
         chrome = self._init_scraping_process()
-        logging.infoé("Begin scraping process")
-
+        logging.info("Begin scraping process")
 
         while True:
 
@@ -74,19 +94,17 @@ class TweetScraper:
                 text_to_search = chrome.find_element('xpath', "//*[contains(text(), 'Essayez de recharger la page.')]")
                 spans = chrome.find_elements('xpath', "//*[contains(text(), 'Réessayer')]")
                 spans[0].click()
+                logging.info("Had to click on reload page button")
             except:
                 pass
 
             # Attendre que la page charge
-            logging.info("wait for loading next page")
             time.sleep(delay_between_page + random.randint(1, 3))
-            logging.debug("scrap page")
 
             # Recupère tous les tweet (et thread) de la page
             try:
                 last_date = self.scrap_page(chrome)
             except Exception as e:
-                logging.error("Error while scrapping page")
                 logging.error(e)
                 raise(e)
 
@@ -96,30 +114,9 @@ class TweetScraper:
                 logging.info("Reached end date")
                 break
             else:
-                logging.debug("Continue scrapping, scrolling page")
                 chrome.execute_script("window.scrollBy(0, 900);")
 
-    def wait_for_connexion(self, chrome, url, max_nb_try=5, delay=30):
-        logging.debug("Checking if user is connected...")
-        chrome.get(url)
-
-        time.sleep(5)
-
-        current_nb_try = 0
-        while True:
-            if current_nb_try >= max_nb_try:
-                logging.error("Max number of try reached to check user connected")
-                return False
-            try:
-                chrome.find_element('xpath', '//button[@aria-label="Menu du compte"]')
-                chrome.find_element('xpath', '//div[@role="presentation"]')
-                logging.info("User is connected")
-                break
-            except Exception as e:
-                logging.debug("User is not connected, waiting for 5 seconds before next check")
-                current_nb_try += 1
-                time.sleep(delay)
-        return True
+    
 
 
     def scrap_page(self, chrome, delay_between_tweet=3, delay_before_closing_tab=2):
@@ -134,37 +131,32 @@ class TweetScraper:
                 tweet_item = self.scrap_tweet(tweet, 'Normal')
             except Exception as e:
                 logging.error("Error while scrapping main tweet")
-                logging.error(e)
                 raise e
+            
             tweet_item.thread_id = thread_id
             time_posted_conv = tweet_item.time_posted
 
             if time_posted_conv in self.already_done_set:
-                logging.info("Already done")
                 continue
             else:
-                logging.info("New tweet to scrap")
                 self.already_done_set.add(time_posted_conv)
 
             self.df.loc[self.nb_row] = tweet_item.to_dict()
             self.nb_row += 1
 
+            # Clique sur le tweet
             ActionChains(chrome).key_down(Keys.CONTROL).click(tweet).key_up(Keys.CONTROL).perform()
             chrome.switch_to.window(chrome.window_handles[-1])
 
-            logging.info("Loading thread")
             time.sleep(delay_between_tweet + random.randint(1, 3))
 
             try:
                 self._scrap_thread(chrome, thread_id)
             except Exception as e:
                 logging.error("Error while scrapping thread")
-                logging.error(e)
                 raise e
 
-            logging.info("Thread scrapper, waiting before closing tab")
             time.sleep(delay_before_closing_tab + random.randint(1, 3))
-            
             
             self.df.to_csv(self.filename, index=False)
             
@@ -187,34 +179,40 @@ class TweetScraper:
                 content = elements[-1].find_elements('xpath', './div[2]/div')
                 header = content[0]
 
-                if self.account_name in header.text:
-                    logging.info("Réponse intéressante")
-                else:
-                    logging.info('Plus de réponses intéressante...')
+                if self.account_name not in header.text:
                     return
 
                 try:
                     subtweet_item = self.scrap_tweet(subtweet, 'Réponse')
                 except Exception as e:
-                    logging.error("Error while scrapping subtweet")
-                    logging.error(e)
+                    logging.error("Error while scrapping tweet in thread")
+                    logging.error(f"Thread link :{chrome.current_url}, reponse index : {index}")   
                     raise(e)
+                
                 subtweet_item.thread_id = thread_id
             
                 self.df.loc[self.nb_row] = subtweet_item.to_dict()
                 self.nb_row += 1
 
                 if last_thread_tweet == "" or index == len(subtweets) - 1:
-                    logging.debug("Last tweet of the thread")
                     return
 
     def scrap_tweet(self, tweet, type):
         tweet_item = Tweet()
-        elements = tweet.find_elements('xpath', './div/div/div')
 
-        content = elements[-1].find_elements('xpath', './div[2]/div')
+        try:
+            elements = tweet.find_elements('xpath', './div/div/div')
+            content = elements[-1].find_elements('xpath', './div[2]/div')
+        except Exception as e:
+            raise "Couldn't get tweet content"
+        
         header = content[0]
-        content_tweet = content[1]
+        time_posted_conv = self._get_tweet_posted_time(header)
+
+        try:
+            content_tweet = content[1]
+        except Exception as e:
+            raise f"Content was not correctly retrieved of tweet posted at {time_posted_conv}"
 
         try:
             # Si le tweet contient un tweet cité, alors mettre la date du tweet cité
@@ -230,12 +228,13 @@ class TweetScraper:
             tweet_item.views = views
             return tweet_item 
         except Exception as e:
-            print("Tweet contains normal text")
             pass
 
-        time_posted_conv = self._get_tweet_posted_time(header)
-        comment, retweet, like, views = self._get_tweet_stats(content[-1])
-        
+        try:
+            comment, retweet, like, views = self._get_tweet_stats(content[-1])
+        except Exception as e:
+            raise f"Stats were not correctly retrieved of tweet posted at {time_posted_conv}"
+
         tweet_item.type = type
         tweet_item.time_posted = time_posted_conv
         tweet_item.content = content_tweet.text  
